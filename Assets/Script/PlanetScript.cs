52095e40d1c5bbe77d0085e189b2d97076455219
Kevin Rudman <kevin.rudman@stonybrook.edu>
2016-11-05 10:48:49 -0400

    #2 Added emtpy implementation of a planet

--- Assets/Script/PlanetScript.cs
+++ Assets/Script/PlanetScript.cs
@@ -1,7 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+/**
+ * Planet Script will include store all the information
+ * related to the planet. 
+ * This includes:
+ * 1. Units (Soldiers, Engineers)
+ * 2. Ships
+ * 3. Resources
+ * 4. Planet State
+ * 5. Planet Type
+ **/
 
 public class PlanetScript : MonoBehaviour {
+
+	public enum PlanetState {
+		Player, Enemy, Contested, Neutral
+	};
+
+	public enum PlanetType {
+		Hybrid, Resource, Soldier, Normal
+	};
+
+
+	public PlanetState state;
+	public PlanetType type;
+	public int soldierCount = 0;
+	public int engineerCount = 0;
+	public int resourceCount = 0;
+	public GameObject[] adjacentPlanet;
+
+	/**
+	 * These will be used for keeping track of the ship.
+	 * Currently, there is no ship implementation so this is commented to avoid errors.
+	**/
+	//private ShipScript[] ships;
+	//private ShipScript selectedShip;
 
 	// Use this for initialization
 	void Start () {
@@ -12,4 +45,29 @@ public class PlanetScript : MonoBehaviour {
 	void Update () {
         transform.Rotate(0, 0, 50 * Time.deltaTime);
     }
+
+	void mineResources() {
+
+	}
+
+	void createShip() {
+
+	}
+
+	void createSoldiers() {
+
+	}
+
+	void createEngineers() {
+
+	}
+		
+	void loadSoldiersToShip() {
+
+	}
+
+	void unLoadSoldiersFromShip() {
+
+	}
+
 }
