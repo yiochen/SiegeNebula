128e7e5e4645a5bfc9b0ac93bec3155412b1a455
Yiou Chen <chen.yiou1992@gmail.com>
2016-11-29 22:04:32 -0500

    added path debug drawing, close #26

---
+++ Assets/Script/EditorUI/PathInspectorScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathInspectorScript : MonoBehaviour {
+    private PathScript path;
+
+	void OnDrawGizmos()
+    {
+        Debug.Log("drawing");
+        Gizmos.color = Color.white;
+        if (!path)
+        {
+            path = GetComponent<PathScript>();
+        }
+        if (path)
+        {
+            Gizmos.DrawLine(path.start.position, path.end.position);
+        }
+    }
+}
