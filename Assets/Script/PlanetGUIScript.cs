b8056f674575d7fcf8238f65544e47277f3c6f2b
Yiou Chen <chen.yiou1992@gmail.com>
2016-12-01 02:55:44 -0500

    ownership decides color of the planet close #37

--- Assets/Script/PlanetGUIScript.cs
+++ Assets/Script/PlanetGUIScript.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class PlanetGUIScript : MonoBehaviour {
     PlanetScript planet;
     public ShipIconScript shipIcon;
+    public Image OwnershipImage;
 	// Use this for initialization
 	void Start () {
         planet = transform.parent.gameObject.GetComponent<PlanetScript>();
@@ -11,6 +12,18 @@ public class PlanetGUIScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        switch (planet.planetOwnership)
+        {
+            case PlanetScript.Ownership.Enemy:
+                OwnershipImage.color = Color.blue;
+                break;
+            case PlanetScript.Ownership.Player:
+                OwnershipImage.color = Color.yellow;
+                break;
+            default:
+                OwnershipImage.color = Color.gray;
+                break;
+        }
         if (planet.ships[Indices.SHIP_PLAYER] != null)
         {
             shipIcon.gameObject.SetActive(true);
