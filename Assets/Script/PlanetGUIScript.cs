8008b481f6b81c2300324de6625bd6b13298a45e
Yiou Chen <chen.yiou1992@gmail.com>
2016-11-30 22:27:14 -0500

    show ship thumbnail when there is a ship, otherwise hide it, close #30

---
+++ Assets/Script/PlanetGUIScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetGUIScript : MonoBehaviour {
+    PlanetScript planet;
+    public ShipIconScript shipIcon;
+	// Use this for initialization
+	void Start () {
+        planet = transform.parent.gameObject.GetComponent<PlanetScript>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (planet.ships[Indices.SHIP_PLAYER] != null)
+        {
+            shipIcon.gameObject.SetActive(true);
+        }
+        else
+        {
+            shipIcon.gameObject.SetActive(false);
+        }
+    }
+}
