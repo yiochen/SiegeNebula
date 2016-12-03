455129ce75845711361e48066929958410965d30
Kevin Rudman <kevin.rudman@stonybrook.edu>
2016-12-03 13:17:45 -0500

    #59 added a bar to indicate the battle

--- Assets/Script/PlanetGUIScript.cs
+++ Assets/Script/PlanetGUIScript.cs
@@ -2,12 +2,25 @@
 using System.Collections;
 using UnityEngine.UI;
 public class PlanetGUIScript : MonoBehaviour {
-    PlanetScript planet;
+    
     public ShipIconScript shipIcon;
     public Image OwnershipImage;
+	public Slider combatBar;
+
+	private Light spotLight;
+	private PlanetScript planet;
+
+	private float flickerTimer = 0f;
+	private const float FLICKER = 0.25f;
+	private bool isWhite = false;
+
 	// Use this for initialization
 	void Start () {
         planet = transform.parent.gameObject.GetComponent<PlanetScript>();
+		spotLight = combatBar.gameObject.GetComponentInChildren<Light> ();
+		spotLight.type = LightType.Spot;
+		spotLight.intensity = 0;
+		combatBar.gameObject.SetActive (false);
 	}
 
 	// Update is called once per frame
@@ -32,5 +45,43 @@ public class PlanetGUIScript : MonoBehaviour {
         {
             shipIcon.gameObject.SetActive(false);
         }
+
+		CombatSliderUpdates ();
     }
+
+	void CombatSliderUpdates() {
+		if (planet.isContested) {
+			combatBar.gameObject.SetActive (true);
+
+			float totalUnits = planet.enemySoldiers.soldierCount + planet.playerSoldiers.soldierCount;
+			combatBar.value = planet.playerSoldiers.soldierCount / totalUnits;
+
+			if (combatBar.normalizedValue <= 0.3f) {
+				spotLight.intensity = (1.0f - combatBar.normalizedValue) * 8;
+				flickerTimer += Time.deltaTime;
+				if (flickerTimer >= FLICKER) {
+					if (isWhite)
+						spotLight.color = Color.red;
+					else
+						spotLight.color = Color.white;
+
+					isWhite = !isWhite;
+					flickerTimer = 0;
+				}
+			} else {
+				ResetLight ();
+				flickerTimer = 0;
+			}
+		} else {
+			ResetLight ();
+			flickerTimer = 0;
+			combatBar.gameObject.SetActive (false);
+		}
+	}
+
+	void ResetLight() {
+		spotLight.intensity = 0;
+		spotLight.color = Color.red;
+		isWhite = false;
+	}
 }
