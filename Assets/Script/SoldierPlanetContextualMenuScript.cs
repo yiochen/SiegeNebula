0ddecea9cfed0814698aaa4039801229662900e2
yiochen <chen.yiou1992@gmail.com>
2016-12-04 01:40:53 -0500

    removed engineer toggle on contextual menu #67

--- Assets/Script/SoldierPlanetContextualMenuScript.cs
+++ Assets/Script/SoldierPlanetContextualMenuScript.cs
@@ -4,7 +4,6 @@ using System;
 using UnityEngine.UI;
 public class SoldierPlanetContextualMenuScript : AbstractPanel {
     public Toggle soldierToggle;
-    public Toggle engineerToggle;
 
     private PlanetScript planetScript;
     protected override void OnActivate()
@@ -16,18 +15,12 @@ public class SoldierPlanetContextualMenuScript : AbstractPanel {
     protected override void CheckForUpdate()
     {
         soldierToggle.isOn = planetScript.GetIsTrainingSoldiers(); // note that, this will trigger createSoldier
-        engineerToggle.isOn = planetScript.GetIsTransingEngineers(); // this too
     }
 
     public void CreateSoldiers(bool value)
     {
         Debug.Log("create soldier " + value);
         planetScript.TrainSoldiers(value);
-    }
-
-    public void CreateEngineers(bool value)
-    {
-        planetScript.TrainEngineers(value);
     }
 
 }
