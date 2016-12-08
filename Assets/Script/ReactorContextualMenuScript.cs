c720a7cf2491fd633fde260bc17e07fea761b695
yiochen <chen.yiou1992@gmail.com>
2016-12-07 21:27:21 -0500

    added ReactorContextualMenu #74

---
+++ Assets/Script/ReactorContextualMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ReactorContextualMenuScript : MonoBehaviour {
+
+    public Toggle reactorToggle;
+    public Slider reactorSlider;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
