7c66a45b823b8034a659892e2bbc7585ae900bf1
yiochen <chen.yiou1992@gmail.com>
2016-12-15 22:36:55 -0500

    created notification #103

---
+++ Assets/Script/NotificationBoardScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NotificationBoardScript : Singleton<NotificationBoardScript> {
+
+    //public float left = 12.2f;
+    //public float bottom = 55.6f;
+
+    //public float lineHeight = 18.5f;
+    public Text notification;
+    private float alpha;
+
+    private CanvasRenderer notificationRenderer;
+
+    void Start()
+    {
+        notificationRenderer = notification.GetComponent<CanvasRenderer>();
+    }
+
+    public void PushMessage(string message)
+    {
+        notificationRenderer.SetAlpha(1.0f);
+        notification.text = message;
+        notification.CrossFadeAlpha(0.0f, 1.0f, true);
+    }
+}
