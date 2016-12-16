2e56e69d1f28dc8bf8af8bf9c8ebfce4ff6fc35c
yiochen <chen.yiou1992@gmail.com>
2016-12-15 20:19:03 -0500

    added splash screen #85

---
+++ Assets/Script/SplashScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour {
+
+    public float waitTime = 1.0f;
+    public SceneFadingScript fade;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("NextLevel");
+	}
+	
+	IEnumerator NextLevel()
+    {
+
+        yield return new WaitForSeconds(waitTime);
+        
+        yield return new WaitForSeconds(fade.FadeOut());
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
