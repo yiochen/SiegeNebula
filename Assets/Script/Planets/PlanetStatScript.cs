d7ccbda740a36eb868eb4830b678a02cef5e2c68
yiochen <chen.yiou1992@gmail.com>
2018-02-13 01:06:20 -0800

    test using xml

---
+++ Assets/Script/Planets/PlanetStatScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class PlanetStatScript : MonoBehaviour
+{
+
+    public PlanetScriptable planetType;
+    public TextAsset stats;
+    // Use this for initialization
+
+    void ParseXML(string xmlString)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(xmlString);
+        Debug.Log(xmlDoc.FirstChild.Name);
+    }
+
+    void Start()
+    {
+        ParseXML(stats.ToString());
+    }
+}
