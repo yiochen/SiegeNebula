fb4e8543af031977f69645b3d57e0d30b8de88ed
yiochen <chen.yiou1992@gmail.com>
2018-02-13 00:05:57 -0800

    changed gizmos style for planet

---
+++ Assets/Script/EditorUI/PlanetInspectorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetInspectorScript : MonoBehaviour
+{
+    public Color color = Color.blue;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, 1.0f);
+    }
+}
